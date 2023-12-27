c88b4c5f063026fa3cda14a87989d0bd73a43a9c
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-12-27 14:53:43 -0800

    feat: add system tray and background process

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTrayEvent, SystemTrayMenuItem};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +10,45 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let mut hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    hide = hide.accelerator("h".to_string());
+    let mut show = CustomMenuItem::new("show".to_string(), "Show");
+    show = show.accelerator("s".to_string());
+    let mut quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    quit = quit.accelerator("q".to_string());
+    let tray_menu = tauri::SystemTrayMenu::new()
+        .add_item(hide)
+        .add_item(show)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let system_tray = tauri::SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| {
+            if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+                match id.as_str() {
+                    "hide" => {
+                        let window = app.get_window("main").unwrap();
+                        window.hide().unwrap();
+                    }
+                    "show" => {
+                        let window = app.get_window("main").unwrap();
+                        window.show().unwrap();
+                    }
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                }
+            }
+        })
+        .on_window_event(|event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
