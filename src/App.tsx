41ccab3c14074cf65733d6b8a277f6b99be87b39
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-01-08 17:01:52 -0800

    feat: create xy plot with d3

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as d3 from "d3";
 import { invoke } from "@tauri-apps/api";
 import { listen } from "@tauri-apps/api/event";
 import {
@@ -6,9 +7,13 @@ import {
   unregister,
 } from "@tauri-apps/api/globalShortcut";
 import { sendNotification } from "@tauri-apps/api/notification";
-import { createSignal, onCleanup } from "solid-js";
+import { createEffect, createSignal, onCleanup } from "solid-js";
 
 function App() {
+  let div!: HTMLDivElement;
+  const [colors, setColors] = createSignal(
+    Array.from({ length: 25 }, (_, i) => (i + 1) * 1000),
+  );
   const [color, setColor] = createSignal(5900);
   const [brightness, setBrightness] = createSignal(100);
   const updateRedshift = () => {
@@ -67,6 +72,94 @@ function App() {
     setColor(color() - 100);
     updateRedshift();
   });
+  createEffect(() => {
+    const width = 700;
+    const height = 600;
+    const margin = { top: 20, right: 20, bottom: 25, left: 30 };
+    const svg = d3
+      .select(div)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, 24])
+      .range([margin.left, width - margin.right]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([1000, 25000])
+      .range([height - margin.bottom, margin.top]);
+    const line = d3
+      .line()
+      .x((_, i) => xScale(i))
+      .y((d) => yScale(d));
+    svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(
+        d3
+          .axisLeft(yScale)
+          .ticks(18)
+          .tickFormat((d) => `${d / 1000}k`),
+      )
+      .selectAll("line")
+      .attr("stroke", "lightgray")
+      .attr("stroke-opacity", 0.7);
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(
+        d3
+          .axisBottom(xScale)
+          .ticks(12)
+          .tickFormat((d) => {
+            if (d === 0) return "12am";
+            if (d < 12) return `${Number(d)}am`;
+            if (d === 12) return "12pm";
+            return `${d - 12}pm`;
+          }),
+      )
+      .selectAll("line")
+      .attr("stroke", "lightgray")
+      .attr("stroke-opacity", 0.7);
+    svg
+      .selectAll(".hline")
+      .data(d3.range(1000, 26000, 1000))
+      .enter()
+      .append("line")
+      .attr("class", "hline")
+      .attr("x1", margin.left)
+      .attr("x2", width - margin.right)
+      .attr("y1", (d) => yScale(d))
+      .attr("y2", (d) => yScale(d))
+      .attr("stroke", "lightgray")
+      .attr("stroke-opacity", 0.7);
+    svg
+      .selectAll(".vline")
+      .data(d3.range(1, 25, 1))
+      .enter()
+      .append("line")
+      .attr("class", "vline")
+      .attr("x1", (d) => xScale(d))
+      .attr("x2", (d) => xScale(d))
+      .attr("y1", margin.top)
+      .attr("y2", height - margin.bottom)
+      .attr("stroke", "lightgray")
+      .attr("stroke-opacity", 0.7);
+    svg
+      .append("g")
+      .attr("class", "data-group")
+      // eslint-disable-next-line solid/reactivity
+      .call((group) => {
+        group
+          .append("path")
+          .datum(colors())
+          .attr("fill", "none")
+          .attr("stroke", "dodgerblue")
+          .attr("stroke-width", 2)
+          .attr("d", line);
+      });
+  });
   onCleanup(() => {
     unlisten.then((unlisten) => unlisten())!;
   });
@@ -93,6 +186,10 @@ function App() {
           setBrightness(Number(e.currentTarget.value));
           updateRedshift();
         }}
+      />
+      <div
+        class="ml-10 w-fit rounded bg-gray-600 shadow shadow-black"
+        ref={div}
       />
     </div>
   );
