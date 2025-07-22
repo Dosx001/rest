626f41c277f1d73a5c1bd653f6592b9373816658
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2025-07-22 16:54:39 -0700

    feat: create client

---
+++ src/client.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+
+const posix = std.posix;
+
+pub fn init() !void {
+    var sa = posix.Sigaction{
+        .handler = .{ .handler = quit },
+        .mask = posix.empty_sigset,
+        .flags = 0,
+    };
+    posix.sigaction(posix.SIG.HUP, &sa, null);
+    posix.sigaction(posix.SIG.INT, &sa, null);
+    posix.sigaction(posix.SIG.QUIT, &sa, null);
+    posix.sigaction(posix.SIG.TERM, &sa, null);
+    sa.handler = .{ .handler = exit };
+    posix.sigaction(posix.SIG.ILL, &sa, null);
+    posix.sigaction(posix.SIG.ABRT, &sa, null);
+    posix.sigaction(posix.SIG.SEGV, &sa, null);
+    const fd = std.posix.socket(std.posix.AF.UNIX, std.posix.SOCK.DGRAM, 0) catch |e| {
+        std.log.err("socket failed: {}", .{e});
+        return;
+    };
+    defer std.posix.close(fd);
+    var addr: std.posix.sockaddr.un = undefined;
+    addr.family = std.posix.AF.UNIX;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const path = @import("path.zig").getPath(allocator);
+    defer allocator.free(path);
+    @memcpy(addr.path[0..path.len], path);
+    var buf: [6]u8 = .{ 'H', 'E', 'L', 'L', 'O', '!' };
+    _ = std.posix.sendto(fd, &buf, 0, @ptrCast(&addr), @intCast(path.len + 2)) catch |e| {
+        std.log.err("sendto failed: {}", .{e});
+    };
+}
+
+fn quit(_: i32) callconv(.C) void {
+    posix.exit(0);
+}
+
+fn exit(signal: i32) callconv(.C) void {
+    switch (signal) {
+        posix.SIG.ILL => std.log.err("Illegal instruction", .{}),
+        posix.SIG.ABRT => std.log.err("Error program aborted", .{}),
+        posix.SIG.SEGV => std.log.err("Segmentation fault", .{}),
+        else => {},
+    }
+    posix.exit(1);
+}
+
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,21 @@
 const std = @import("std");
 const server = @import("server.zig");
+const client = @import("client.zig");
 
 pub fn main() !void {
+    if (1 < std.os.argv.len) {
+        switch (std.os.argv[1][0]) {
+            'c' => {
+                if (std.mem.eql(
+                    u8,
+                    std.mem.span(std.os.argv[1]),
+                    "client",
+                )) return client.init();
+                return error.InvalidArgument;
+            },
+            else => {},
+        }
+        return;
+    }
     server.init();
 }
